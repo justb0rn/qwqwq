ece3a76e5a0cfa067a41e49030d10a5e1315dd22
justb0rn <75207784+justb0rn@users.noreply.github.com>
2020-12-06 21:26:34 +0300

    Add files via upload

---
+++ Lesson 4.py
@@ -0,0 +1,36 @@
+import math
+a = int (input())
+b = int (input())
+def NOD(a, b):
+    while a != b:
+        if a > b:
+            a = a - b
+        else:
+            b = b - a
+    print (a)
+    return a
+
+t = NOD(a ,b)
+def NOK(a, b):
+    a = a * b // t
+    print (a)
+
+NOK(a ,b)
+
+number =int(input())
+number2 = number
+factors = []
+d = 2
+
+def Primenumberdecomposition(number ,d):
+    for i in range(2, int(math.sqrt(number)) + 1):
+        while number % i == 0:
+            factors.append(i)
+            number //= i
+
+    if number != 1:
+        factors.append(number)
+
+    print('{} = {}' .format(number2 ,factors))
+
+t = Primenumberdecomposition (number ,d)
